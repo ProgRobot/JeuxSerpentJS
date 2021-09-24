8011c80c857478aef38539d3fc7997dcf6062808
YoucefKF <kfyoucef@gmail.com>
2021-09-24 15:19:47 +0200

    End of objects reminder part

--- testsJS1.js
+++ testsJS1.js
@@ -31,10 +31,82 @@ var a = 10,
 var resultat = multiply2(a, b, c);
 //console.log(resultMultiply);
 
-//*Tables   => in a unique variable we will save several values
+//*Arrays  => in a unique variable we will save several values
 
 var fruits = ["pomme", "orange", "Poire", "Cirtron"];
 
 for (var i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
+
+//We can exploid all possibilties by appliying arrays method on fruits arrray
+
+//*Creating objects
+/*
+var object={
+  propertyName1 : propertyValue1;
+  propertyName2 : propertyVAlue2; 
+}
+*/
+
+//?Dog object creation :)
+/*
+var dog = {
+  name: "Liza",
+  age: 5,
+  color: "pink",
+};
+
+console.log(dog);
+
+for (var property in dog) {
+  console.log(dog[property]);
+}
+
+*/
+
+//?Another manner to create object
+
+var dog = new Object();
+dog.name = "nini";
+dog.color = "white";
+dog.age = 25;
+dog.aboie = function () {
+  var repetition = 0;
+  while (repetition < 3) {
+    console.log("whouaf, whouaf");
+    repetition++;
+  }
+};
+
+dog.aboie();
+
+//*Constructors functions
+
+//?Lets define a a function constructor to create a dog
+
+function Dog(name, color, age) {
+  this.name = name;
+  this.color = color;
+  this.age = age;
+
+  this.aboie = function (dogSound) {
+    var repetition = 0;
+    for (let repetition = 0; repetition < 3; repetition++) {
+      console.log(dogSound);
+    }
+  };
+}
+
+var petitCaniche = new Dog("momo", "white", 15);
+var grosPitbull = new Dog("fofo", "red", 5);
+
+console.log("====================================");
+console.log(petitCaniche);
+console.log("====================================");
+console.log("====================================");
+console.log(grosPitbull.name, grosPitbull.color, grosPitbull.age);
+console.log("====================================");
+
+petitCaniche.aboie("Haouuu");
+grosPitbull.aboie("Aaouuu");
